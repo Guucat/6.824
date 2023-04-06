11cde5a861b7e43bacd4636e38494a7518130b15
shengyi <1027804480@qq.com>
2023-04-06 15:30:34 +0800

    "测试结果更新"

--- src/kvraft/test_test.go
+++ src/kvraft/test_test.go
@@ -418,7 +418,8 @@ func GenericTestSpeed(t *testing.T, part string, maxraftstate int) {
 	if dur > numOps*timePerOp {
 		t.Fatalf("Operations completed too slowly %v/op > %v/op\n", dur/numOps, timePerOp)
 	}
-
+	fmt.Println("dur", dur)
+	fmt.Println(dur / numOps)
 	cfg.end()
 }
 
